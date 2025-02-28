5fa0353ee85a78f76b5af4535ef2573d5e7181f0
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2025-02-28 18:05:37 +0300

    bug fixes for async

--- utils/getRewardOrCommissionArraysFromEvent.ts
+++ utils/getRewardOrCommissionArraysFromEvent.ts
@@ -1,16 +1,24 @@
 
+import async from "async";
+
 // const MOCKUP_VALUE = "169ibc/B011C1A0AD5E717F674BA59FD8E05B2F946E4FD41C9CB3311C95F7ED4B815620,37070ibc/B38AAA0F7A3EC4D7C8E12DFA33FF93205FE7A42738A4B0590E2FF15BC60A612B,100uatom";
 
 export const getOnlyNativeTokenValueFromCommissionOrRewardEvent = function (value: string, callback: (err: string | null, nativeRewardOrCommissionValue: string | null) => any) {
-  
+
   const splitString = value.split(",");
 
-  splitString.forEach(eachWithdrawChunk => {
+  async.timesSeries(splitString.length, (i, next) => {
+    
+    const eachWithdrawChunk = splitString[i];
+
     if (eachWithdrawChunk.includes("uatom")) {
 
       const nativeTokenAmount = eachWithdrawChunk.replace("uatom", "");
       return callback(null, nativeTokenAmount);
+    } else {
+      next();
     }
+  }, (err) => {
+    return callback("native_token_not_found", null)
   })
-  return callback("native_token_not_found", null)
 }
--- utils/getSpecificAttributeOfAnEventFromTxEventsArray.ts
+++ utils/getSpecificAttributeOfAnEventFromTxEventsArray.ts
@@ -1,19 +1,28 @@
 
+import async from "async";
+
 interface EventInterface {
   type: string,
   attributes: any[]
 }
 
 export const getSpecificAttributeOfAnEventFromTxEventsArray = function (events: EventInterface[], specificEventType: string, specificAttributeKey: string, callback: (err: string | null, specificAttributeValue: string | null) => any) {
-  events.forEach((eachEvent: EventInterface) => {
+  
+  async.timesSeries(events.length, (i, next) => {
+    const eachEvent: EventInterface = events[i];
+
     if (eachEvent.type == specificEventType) {
       const attributes = eachEvent.attributes;
       attributes.forEach(eachAttribute => {
         if (eachAttribute.key == specificAttributeKey) {
           return callback(null, eachAttribute.value);
-        } 
+        } else {
+          next();
+        }
       })
     }
+
+  }, (err) => {
+    return callback("not_found", null);
   });
-  return callback("not_found", null);
 }
