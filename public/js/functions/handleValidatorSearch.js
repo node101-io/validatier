39a2219772bf2783c18747aca47a26e5753ecdfc
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2025-03-16 00:04:45 +0300

    minor fix of line appearing inappropriately when no validator result

--- public/js/functions/handleValidatorSearch.js
+++ public/js/functions/handleValidatorSearch.js
@@ -6,8 +6,14 @@ function handleValidatorSearch () {
   
   validatorFilterInput.addEventListener('keyup', (event) => {
 
-    if (!validatorFilterInput.value || validatorFilterInput.value.length < 4) return document.querySelectorAll('.each-validator-wrapper').forEach(each => each.style.display = 'flex');;
-    document.querySelectorAll('.each-validator-wrapper').forEach(each => each.style.display = 'none');
+    if (!validatorFilterInput.value || validatorFilterInput.value.length < 4) return document.querySelectorAll('.each-validator-wrapper').forEach(each => {
+      each.style.display = 'flex';
+      each.nextSibling.style.display = 'unset';
+    });
+    document.querySelectorAll('.each-validator-wrapper').forEach(each => {
+      each.style.display = 'none';
+      each.nextSibling.style.display = 'none';
+    });
     for (let i = 0; i < monikers.length; i++) {
       const eachMoniker = monikers[i];
       const eachOperatorAddress = operatorAddresses[i];
@@ -17,6 +23,7 @@ function handleValidatorSearch () {
         eachOperatorAddress.includes(validatorFilterInput.value.trim().toLowerCase())
       ) {
         document.getElementById(operatorAddresses[i]).style.display = 'flex';
+        document.getElementById(operatorAddresses[i]).nextSibling.style.display = 'unset';
       }
     }
   })
