0ac4a7e2e704434f7a6474d05590be95abd78365
umutsatir <u.satir2021@gtu.edu.tr>
2025-09-23 20:49:55 +0300

    validator page responsive fix

--- frontend/src/app/validator/[operatorAddress]/page.tsx
+++ frontend/src/app/validator/[operatorAddress]/page.tsx
@@ -55,9 +55,9 @@ export default function ValidatorPage({
         <div className="flex flex-col items-center relative overflow-hidden h-screen w-full">
             <div className="flex flex-col w-full items-center relative overflow-x-hidden overflow-y-scroll ml-0 h-screen rounded-0 bg-white transition-all duration-250">
                 <Navbar isValidatorPage={true} />
-                <div className="mt-19 w-250 h-fit px-10">
+                <div className="mt-19 w-full lg:w-[1100px] h-fit lg:px-10">
                     <div className="flex flex-col w-full gap-5 mt-5">
-                        <div className="flex flex-row items-center justify-between w-full gap-0 rounded-3xl px-6 py-7.5 border-[0.5px] border-[#bebee7] bg-[#f5f5ff]">
+                        <div className="flex flex-col gap-5 sm:gap-0 sm:flex-row items-center justify-between w-full rounded-3xl px-6 py-7.5 border-[0.5px] border-[#bebee7] bg-[#f5f5ff]">
                             {/* Validator Info */}
                             <div className="flex items-center gap-2.5">
                                 <img
@@ -100,87 +100,93 @@ export default function ValidatorPage({
                                 </a>
                             </div>
                         </div>
-                        <div className="flex flex-row flex-nowrap justify-between gap-5 overflow-x-visible ml-0 p-0">
+                        <div className="flex flex-row flex-nowrap justify-start gap-5 overflow-y-hidden overflow-x-auto lg:overflow-x-hidden no-scrollbar ml-0 p-0">
                             {/* Network Summary */}
-                            <NetworkSummary
-                                leftColumn={
-                                    <>
-                                        <div className="flex text-xl font-normal text-[#7c70c3] text-nowrap items-center">
-                                            Self Stake
-                                        </div>
-                                        <div
-                                            className="text-[28px] font-bold text-[#49306f] leading-3 mb-0.5 text-nowrap"
-                                            id="summary-self-stake-amount-native"
-                                        >
-                                            {validator.selfStake
-                                                ? `${formatNumber(
-                                                      validator.selfStake
-                                                  )} ATOM`
-                                                : "- ATOM"}
-                                        </div>
-                                        <div
-                                            className="font-medium text-[20px] text-[#7c70c3]"
-                                            id="summary-self-stake-amount-usd"
-                                        >
-                                            {validator.selfStake &&
-                                            validator.selfStake > 0
-                                                ? `$${formatNumber(
-                                                      atomToUSD(
+                            <div className="shrink-0">
+                                <NetworkSummary
+                                    leftColumn={
+                                        <>
+                                            <div className="flex text-xl font-normal text-[#7c70c3] text-nowrap items-center">
+                                                Self Stake
+                                            </div>
+                                            <div
+                                                className="text-[28px] font-bold text-[#49306f] leading-3 mb-0.5 text-nowrap"
+                                                id="summary-self-stake-amount-native"
+                                            >
+                                                {validator.selfStake
+                                                    ? `${formatNumber(
                                                           validator.selfStake
-                                                      )
-                                                  )}`
-                                                : "-"}
+                                                      )} ATOM`
+                                                    : "- ATOM"}
+                                            </div>
+                                            <div
+                                                className="font-medium text-[20px] text-[#7c70c3]"
+                                                id="summary-self-stake-amount-usd"
+                                            >
+                                                {validator.selfStake &&
+                                                validator.selfStake > 0
+                                                    ? `$${formatNumber(
+                                                          atomToUSD(
+                                                              validator.selfStake
+                                                          )
+                                                      )}`
+                                                    : "-"}
+                                            </div>
+                                        </>
+                                    }
+                                    rightColumn={
+                                        <div className="text-nowrap mt-auto text-[#7c70c3] font-medium text-base">
+                                            7th out of 215
                                         </div>
-                                    </>
-                                }
-                                rightColumn={
-                                    <div className="text-nowrap mt-auto text-[#7c70c3] font-medium text-base">
-                                        7th out of 215
-                                    </div>
-                                }
-                            />
-                            <NetworkSummary
-                                leftColumn={
-                                    <>
-                                        <div className="flex text-xl font-normal text-[#7c70c3] text-nowrap items-center">
-                                            Percentage sold
+                                    }
+                                />
+                            </div>
+                            <div className="shrink-0">
+                                <NetworkSummary
+                                    leftColumn={
+                                        <>
+                                            <div className="flex text-xl font-normal text-[#7c70c3] text-nowrap items-center">
+                                                Percentage sold
+                                            </div>
+                                            <div
+                                                className="text-[36px] leading-[22px] font-bold text-[#49306f] text-nowrap mb-0.5"
+                                                id="summary-percentage-sold-native"
+                                            >
+                                                24%
+                                            </div>
+                                            <div className="font-medium text-[20px] text-[#7c70c3]"></div>
+                                        </>
+                                    }
+                                    rightColumn={
+                                        <div className="text-nowrap mt-auto text-[#7c70c3] font-medium text-base">
+                                            1st out of 215
                                         </div>
-                                        <div
-                                            className="text-[36px] leading-[22px] font-bold text-[#49306f] text-nowrap mb-0.5"
-                                            id="summary-percentage-sold-native"
-                                        >
-                                            24%
+                                    }
+                                />
+                            </div>
+                            <div className="shrink-0">
+                                <NetworkSummary
+                                    leftColumn={
+                                        <>
+                                            <div className="flex text-xl font-normal text-[#7c70c3] text-nowrap items-center">
+                                                Commission
+                                            </div>
+                                            <div
+                                                className="text-[36px] leading-[22px] font-bold text-[#49306f] text-nowrap mb-0.5"
+                                                id="summary-average-self-stake-ratio-native"
+                                            >
+                                                5%
+                                            </div>
+                                            <div className="font-medium text-[20px] text-[#7c70c3]"></div>
+                                        </>
+                                    }
+                                    rightColumn={
+                                        <div className="text-nowrap mt-auto text-[#7c70c3] font-medium text-base">
+                                            Fee from rewards
                                         </div>
-                                        <div className="font-medium text-[20px] text-[#7c70c3]"></div>
-                                    </>
-                                }
-                                rightColumn={
-                                    <div className="text-nowrap mt-auto text-[#7c70c3] font-medium text-base">
-                                        1st out of 215
-                                    </div>
-                                }
-                            />
-                            <NetworkSummary
-                                leftColumn={
-                                    <>
-                                        <div className="flex text-xl font-normal text-[#7c70c3] text-nowrap items-center">
-                                            Commission
-                                        </div>
-                                        <div
-                                            className="text-[36px] leading-[22px] font-bold text-[#49306f] text-nowrap mb-0.5"
-                                            id="summary-average-self-stake-ratio-native"
-                                        >
-                                            5%
-                                        </div>
-                                        <div className="font-medium text-[20px] text-[#7c70c3]"></div>
-                                    </>
-                                }
-                                rightColumn={
-                                    <div className="text-nowrap mt-auto text-[#7c70c3] font-medium text-base">
-                                        Fee from rewards
-                                    </div>
-                                }
-                            />
+                                    }
+                                />
+                            </div>
                         </div>
                         <GraphMetrics
                             firstSeries={seriesDelegation}
