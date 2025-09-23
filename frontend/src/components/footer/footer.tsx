b174c74993489a673149d957efce15e82aca7331
umutsatir <u.satir2021@gtu.edu.tr>
2025-09-23 15:23:16 +0300

    footer responsiveness added

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
             <div className="footer-svg-wrapper w-[calc(100%+400px)] min-h-[300px] max-h-[300px] absolute left-0 bottom-0 overflow-hidden my-0 -mx-50 z-0">
                 <FooterSVG />
             </div>
-            <div className="flex w-full flex-row justify-between items-center relative z-100">
-                <div className="flex flex-row items-center gap-3">
+            <div className="flex w-full flex-col sm:flex-row justify-between items-center gap-3 sm:gap-0 relative z-100">
+                <div className="flex flex-row flex-wrap items-center gap-3">
                     <a href="mailto:hello@node101.io" target="_blank">
                         hello@node101.io
                     </a>
@@ -17,7 +17,7 @@ export default function Footer() {
                         node101.io
                     </a>
                 </div>
-                <div className="flex flex-row items-center gap-3">
+                <div className="flex flex-row items-center gap-3 mt-2 sm:mt-0">
                     <a href="https://x.com/node_101" target="_blank">
                         X
                     </a>
@@ -26,19 +26,19 @@ export default function Footer() {
                     </a>
                 </div>
             </div>
-            <div className="flex items-center gap-4 -mt-6 relative">
-                <div className="xl:h-[110px] xl:mt-0 lg:h-[85px] lg:-mt-1 aspect-24/15">
-                    <LogoSVG fill="#361661" className="w-full h-full mt-4" />
+            <div className="flex items-end gap-4 -mt-2 sm:-mt-6 relative">
+                <div className="h-[36px] sm:h-[72px] lg:h-[84px] xl:h-[110px] aspect-24/15 mb-0.5 lg:mb-1 xl:mb-0">
+                    <LogoSVG fill="#361661" className="w-full h-full" />
                 </div>
-                <div className="xl:text-[180px]/[140px] lg:text-[140px]/[120px] font-[800] conic-gradient-bg">
+                <div className="text-[56px]/[48px] sm:text-[120px]/[96px] lg:text-[140px]/[120px] xl:text-[180px]/[140px] font-[800] conic-gradient-bg">
                     VALIDATIER
                 </div>
             </div>
-            <div className="flex w-full flex-row justify-between items-center relative z-100">
-                <div className="flex flex-row items-center gap-3">
+            <div className="flex w-full flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0 relative z-100">
+                <div className="flex flex-row items-center gap-3 order-2 sm:order-1">
                     <span>Designed and developed by node101</span>
                 </div>
-                <div className="flex flex-row items-center gap-3">
+                <div className="flex flex-row items-center gap-3 order-1 sm:order-2">
                     <span>© 2025. All rights reserved</span>
                 </div>
             </div>
